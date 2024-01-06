2b157412d655f94b2ed16fcd9b33e9864b1c11e5
sknutsen <sondreknutsen1@gmail.com>
2024-01-06 18:52:23 +0100

    removed unused imports from shell.nix

--- shell.nix
+++ shell.nix
@@ -2,33 +2,8 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   my-python-packages = ps: with ps; [
-    requests
-    msal
-    google-api-python-client
-    google-auth-httplib2
-    google-auth-oauthlib
     icalendar
     psycopg2
-    (
-      buildPythonPackage rec {
-        pname = "O365";
-        version = "2.0.31";
-        src = fetchPypi {
-          inherit pname version;
-          sha256 = "sha256-BbNnpY5HzFNvWKm78loaEAg6m+PF0v1+/EXJrQFbh9A=";
-        };
-        doCheck = false;
-        propagatedBuildInputs = [
-          # Specify dependencies
-          pkgs.python3Packages.oauthlib
-          pkgs.python3Packages.requests_oauthlib
-          pkgs.python3Packages.stringcase
-          pkgs.python3Packages.tzlocal
-          pkgs.python3Packages.dateutil
-          pkgs.python3Packages.beautifulsoup4
-        ];
-      }
-    )
   ];
   my-python = pkgs.python3.withPackages my-python-packages;
 in my-python.env
